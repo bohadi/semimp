677aefad6edbd14763f5b5360c86457981041f17
bohadi <bohadi@protonmail.com>
2018-01-17 22:49:55 -0600

    read input

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,10 @@ import System.IO(hFlush, stdout)
 
 main :: IO ()
 main = do
-    --putStr "input size: "
-    --hFlush stdout
-    --input <- getLine
-    --let size   = read input::Int
-    let size   = 30200100
+    putStr "input size: "
+    hFlush stdout
+    input <- getLine
+    let size   = read input::Int
+    --let size   = 30200100
         output = run $ sumPgm size
     putStrLn $ show output
